fba0de90261945f693b4bd572295350d30512327
Скрябин Павел Николаевич <skryabin.p@coral-meat.ru>
2023-11-01 14:56:39 +0300

    SushiSwap v2 & v3

---
+++ DEX/SushiSwapV2.py
@@ -0,0 +1,24 @@
+from UniswapV2 import UniswapV2
+
+
+class SushiSwapV2(UniswapV2):
+    router_abi = "SushiSwap-v2/Router02"
+    graph_endpoint = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange"
+
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+    import time
+
+    load_dotenv()
+    net = os.environ['INFURA_MAINNET']
+    client = SushiSwapV2(net)
+    print(client.pair_list)
+    client.update_price_book(1)
+    print(client.price_book)
+    time.sleep(1)
+    client.update_price_book(1)
+    print(client.price_book)
+    client.update_price_book(1)
+    print(client.price_book)
---
+++ DEX/SushiSwapV3.py
@@ -0,0 +1,27 @@
+from UniswapV3 import UniswapV3
+
+
+class SushiSwapV3(UniswapV3):
+    quoter_ver = "v2"
+    abi_folder = "SushiSwap-v3"
+    graph_endpoint = "https://api.thegraph.com/subgraphs/name/sushi-v3/v3-ethereum"
+    multicall_abi = "ERC20/multicall"
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+    from utils import get_function_abi
+    import time
+
+    load_dotenv()
+    net = os.environ['INFURA_MAINNET']
+
+    client = SushiSwapV3(net, fee=3000)
+    print(client.pair_list)
+    client.update_price_book(1)
+    print(client.price_book)
+    time.sleep(1)
+    client.update_price_book(1)
+    print(client.price_book)
+    client.update_price_book(1)
+    print(client.price_book)
